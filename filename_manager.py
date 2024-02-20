6fc5b1ffdad51dd2741cf0d99eebc983fcd2f888
Alex Warren <alexander.ch.warren@gmail.com>
2024-02-20 17:27:34 -0500

    Prepend all files in given directory

--- filename_manager.py
+++ filename_manager.py
@@ -3,14 +3,26 @@ import pathlib
 
 class FilenameManager:
     def __init__(self):
-        directory = pathlib.Path(self.__prompt_directory_path())
-        for file in directory.walk():
-            print(file)
+        self.directory = pathlib.Path(self.__prompt_directory_path())
+        prepend = self.__prompt_prepend()
+        self.prepend_files(prepend)
+
+    def prepend_files(self, prepend):
+        for file in self.directory.iterdir():
+            # Skip if it's a directory
+            if file.is_dir():
+                continue
+
+            new_file = file.with_stem(prepend + file.stem)
+            file.replace(new_file)
 
     def __prompt_directory_path(self):
         directory_path = input("Please enter directory path: ")
-        print(f"You entered >>>{directory_path}<<<")
         return directory_path
+
+    def __prompt_prepend(self):
+        prepend = input("Please enter prepend string: ")
+        return prepend
 
 
 if __name__ == "__main__":
